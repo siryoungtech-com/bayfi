3f7dd19bb981b9349496d0237ce5eac605494f36
Onadiran Bayonle <onadiranbayonle358@gmail.com>
2025-02-17 13:47:41 +0100

    Initial commit : set up the BayFi project

---
+++ src/main/java/com/bayfi/BayfiApplication.java
@@ -0,0 +1,13 @@
+package com.bayfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayfiApplication.class, args);
+	}
+
+}
